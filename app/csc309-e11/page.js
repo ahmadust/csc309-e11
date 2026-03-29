11bec7cdea52c9582f886558880b9e139c3ed9d8
takriti1 <ahmad.takriti@mail.utoronto.ca>
2026-03-29 03:15:49 -0400

    done

--- app/csc309-e11/page.js
+++ app/csc309-e11/page.js
@@ -1,11 +1,9 @@
-// Replace with your UTorID if different (must match script.js).
+
 const UTORID = "takriti1";
 
+const AUTO_TESTER_TEXT = `Hi. I am ${UTORID}, and I have learned how to develop a full-stack web app
+and deploy it!`;
+
 export default function AutoTesterPage() {
-  return (
-    <div id="auto-tester">
-      Hi. I am {UTORID}, and I have learned how to develop a full-stack web app
-      and deploy it!
-    </div>
-  );
+  return <div id="auto-tester">{AUTO_TESTER_TEXT}</div>;
 }
