7d7298d11652c11881dbaf32f2afd08724bf1114
takriti1 <ahmad.takriti@mail.utoronto.ca>
2026-03-29 03:26:27 -0400

    fix?

--- app/csc309-e11/page.js
+++ app/csc309-e11/page.js
@@ -1,8 +1,6 @@
-
 const UTORID = "takriti1";
 
-const AUTO_TESTER_TEXT = `Hi. I am ${UTORID}, and I have learned how to develop a full-stack web app
-and deploy it!`;
+const AUTO_TESTER_TEXT = `Hi. I am ${UTORID}, and I have learned how to develop a full-stack web app and deploy it!`;
 
 export default function AutoTesterPage() {
   return <div id="auto-tester">{AUTO_TESTER_TEXT}</div>;
